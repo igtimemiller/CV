ab940469cddd2920109fea4efa2a3fc662d690c4
ALUMNADO\igtime <igtime@florida-uni.es>
2019-03-25 10:23:21 +0100

    Añadiendo Responsive

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function clickdatosc(bal){
 }
 
 function clickformacion(bal){
-    textohelp.innerHTML = '<h2> Titulación de BACHILLER CIENTÍFICO</h2> Descargala <a href="https://drive.google.com/uc?export=download&id=1YQUVwjctgB1Ookx1Az66-YbJZ4pUGQlo">aquí</a> <br><br> <h3>Actualmente cursando Grado Superior en Desarrollo de Aplicaciones Web </h3> Más información <a href="http://www.floridauniversitaria.es/es-ES/OfertaAcademica/ciclosformativos/Paginas/desarrollo-aplicaciones-web.aspx?Perfil=Futuro+Estudiante" target="_blank">aquí</a> ';
+    textohelp.innerHTML = '<div class="topcero"><h3> Titulación de BACHILLER CIENTÍFICO</h3> &nbsp&nbsp Descargala <a href="https://drive.google.com/uc?export=download&id=1YQUVwjctgB1Ookx1Az66-YbJZ4pUGQlo">aquí</a> <br> <h3>Actualmente cursando Grado Superior en Desarrollo de Aplicaciones Web </h3> &nbsp&nbsp Más información <a href="http://www.floridauniversitaria.es/es-ES/OfertaAcademica/ciclosformativos/Paginas/desarrollo-aplicaciones-web.aspx?Perfil=Futuro+Estudiante" target="_blank">aquí</a> </div>';
 }
 
 function clickidiomas(bal){
